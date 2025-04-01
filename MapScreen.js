d589d0607e40f3a8ed5d4a16d5df6fceac86a8bf
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-04-01 15:28:25 +0200

    Added map

--- MapScreen.js
+++ MapScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import MapView from "react-native-maps";
 
 export default function MapScreen() {
     return (
         <View style={styles.container}>
             <Text>Map Screen</Text>
+            <MapView
+                style={styles.map}
+            />
         </View>
     );
 }
@@ -15,4 +19,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    map: {
+        width: '100%',
+        height: '50%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+// Fetch https://github.com/Mathijs-04/PRG7-JSON/blob/main/gyms.json
