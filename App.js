4d790547ed33ef1891bb675e70fbb61939d6cbe7
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-04-01 15:18:11 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import GymsScreen from './GymsScreen';
+import MapScreen from './MapScreen';
+import HomeScreen from './HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const Tab = createBottomTabNavigator();
+
+    return (
+        <NavigationContainer>
+            <Tab.Navigator id={1}>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Gyms" component={GymsScreen} />
+                <Tab.Screen name="Map" component={MapScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
