985034a6f99c92eacaa15d50bf7025e45fd2e2b2
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-04-02 10:39:33 +0200

    Added fetch

--- MapScreen.js
+++ MapScreen.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import MapView from "react-native-maps";
+import {useState, useEffect} from "react";
 
 export default function MapScreen() {
+    const [gyms, setGyms] = useState([]);
+
+    useEffect(() => {
+        getGyms();
+    }, []);
+
+    async function getGyms() {
+        const url = "https://raw.githubusercontent.com/Mathijs-04/PRG7-JSON/main/gyms.json";
+        try {
+            const response = await fetch(url, {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const data = await response.json();
+            setGyms(data);
+            console.log(gyms);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Text>Map Screen</Text>
@@ -27,5 +53,3 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
-
-// Fetch https://github.com/Mathijs-04/PRG7-JSON/blob/main/gyms.json
